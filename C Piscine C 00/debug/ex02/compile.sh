edc120485d24794a8041115b77ab14d55261d8e7
asdeadpan <as@deadpan.pro>
2023-03-15 12:38:39 +0000

    ex02
    c piscine c 00
    +ex02

---
+++ C Piscine C 00/debug/ex02/compile.sh
@@ -0,0 +1,2 @@
+gcc	-Wall	-Wextra	-Werror ft_print_reverse_alphabet.c	main.c
+./a.out
---
+++ C Piscine C 00/debug/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	l;
+
+	l = 'z';
+	while (l >= 'a')
+	{
+		write(1, &l, 1);
+		--l;
+	}
+}
